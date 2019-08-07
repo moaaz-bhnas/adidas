e6e957ffb197ed93faf25965273efb53727b979f
moaaz-bhnas <moaaz_bs@yahoo.com>
2019-08-07 03:50:11 +0200

    Updates

---
+++ precache-manifest.6e7838b4cf65eb1b66e3bc56eb0b0963.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "510d32d93e04a611bc68ce85c3f38168",
+    "url": "/adidas/index.html"
+  },
+  {
+    "revision": "3c8914f9017cc8b02cc9",
+    "url": "/adidas/static/css/2.d72df4ce.chunk.css"
+  },
+  {
+    "revision": "c25a0e8519be54f975ca",
+    "url": "/adidas/static/css/main.bd50cd26.chunk.css"
+  },
+  {
+    "revision": "3c8914f9017cc8b02cc9",
+    "url": "/adidas/static/js/2.261cff38.chunk.js"
+  },
+  {
+    "revision": "c25a0e8519be54f975ca",
+    "url": "/adidas/static/js/main.a6a0663d.chunk.js"
+  },
+  {
+    "revision": "d36513316cc0aca74692",
+    "url": "/adidas/static/js/runtime~main.46dd9c9f.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/adidas/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/adidas/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/adidas/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/adidas/static/media/slick.f97e3bbf.svg"
+  }
+]);
