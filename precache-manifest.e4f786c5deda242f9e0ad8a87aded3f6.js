24d6f5ad751f10ae1ed6dbc604e9570fb904556b
moaaz-bhnas <moaaz_bs@yahoo.com>
2019-08-24 21:58:55 +0200

    Updates

---
+++ precache-manifest.e4f786c5deda242f9e0ad8a87aded3f6.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d4786c7b7a0cdf651d046580815a8ac",
+    "url": "/adidas/index.html"
+  },
+  {
+    "revision": "511d03828eea4651e805",
+    "url": "/adidas/static/css/2.d72df4ce.chunk.css"
+  },
+  {
+    "revision": "e43034aa1446adf840f5",
+    "url": "/adidas/static/css/main.c8860c5c.chunk.css"
+  },
+  {
+    "revision": "511d03828eea4651e805",
+    "url": "/adidas/static/js/2.11412437.chunk.js"
+  },
+  {
+    "revision": "e43034aa1446adf840f5",
+    "url": "/adidas/static/js/main.462e4003.chunk.js"
+  },
+  {
+    "revision": "d36513316cc0aca74692",
+    "url": "/adidas/static/js/runtime~main.46dd9c9f.js"
+  },
+  {
+    "revision": "ba6ec07bc8f7ac138d630038b7bfce1c",
+    "url": "/adidas/static/media/banner.ba6ec07b.png"
+  },
+  {
+    "revision": "5d05c11b1daad2d77a30425ca514e2f5",
+    "url": "/adidas/static/media/blurred-shop.5d05c11b.png"
+  },
+  {
+    "revision": "187036bb90c01f90491b891dd7d5b64f",
+    "url": "/adidas/static/media/bottom-banner.187036bb.png"
+  },
+  {
+    "revision": "a6dc6a7ea2f81634cbe14da1d0ed9c84",
+    "url": "/adidas/static/media/bottom-banner2.a6dc6a7e.png"
+  },
+  {
+    "revision": "b8678d86d8639b768ccd22c40712b2c3",
+    "url": "/adidas/static/media/category-1.b8678d86.png"
+  },
+  {
+    "revision": "cdc696e6baa811160dbb115294f8f9cf",
+    "url": "/adidas/static/media/category-2.cdc696e6.png"
+  },
+  {
+    "revision": "1360559903c68d0161018e155f43729b",
+    "url": "/adidas/static/media/collection1.13605599.png"
+  },
+  {
+    "revision": "ed944430691437ed6b56d44898765e84",
+    "url": "/adidas/static/media/collection2.ed944430.png"
+  },
+  {
+    "revision": "e6820e9419cdeeb319c465a12c1367e4",
+    "url": "/adidas/static/media/color-2.e6820e94.png"
+  },
+  {
+    "revision": "7cb5466df2b39f7b7ccaa8a8e4f5e441",
+    "url": "/adidas/static/media/coordinate.7cb5466d.png"
+  },
+  {
+    "revision": "7627f28d6c89d31e889b2ff7fe5ad6a3",
+    "url": "/adidas/static/media/logo.7627f28d.png"
+  },
+  {
+    "revision": "feec89295356975e2854dec08c4b5334",
+    "url": "/adidas/static/media/product.feec8929.png"
+  },
+  {
+    "revision": "d13c783802407ba23eea664724959ade",
+    "url": "/adidas/static/media/product2.d13c7838.png"
+  },
+  {
+    "revision": "7b0e0df96d3cf8b6180329ff26b67575",
+    "url": "/adidas/static/media/re-sports.7b0e0df9.png"
+  },
+  {
+    "revision": "4a06dda9e00d35caba220d3b98b526a9",
+    "url": "/adidas/static/media/shop.4a06dda9.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/adidas/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/adidas/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/adidas/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/adidas/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "3db670035ad9fdd3ed74f5f05868d96b",
+    "url": "/adidas/static/media/supercourt.3db67003.png"
+  },
+  {
+    "revision": "736dc853ef369d9cd3658ebf53e57ca4",
+    "url": "/adidas/static/media/top-banner2.736dc853.png"
+  },
+  {
+    "revision": "48c90e2c3292b8760f30eca7919c0227",
+    "url": "/adidas/static/media/toy-story.48c90e2c.png"
+  },
+  {
+    "revision": "a29fcfcd8b6e7671d88102a104c93475",
+    "url": "/adidas/static/media/trending-1.a29fcfcd.png"
+  },
+  {
+    "revision": "a58cc826c22f8dc8ee99725cdeb90604",
+    "url": "/adidas/static/media/trending-2.a58cc826.png"
+  },
+  {
+    "revision": "4d67151417bc6fbe366b3126a7ec3c9f",
+    "url": "/adidas/static/media/trending-3.4d671514.png"
+  },
+  {
+    "revision": "70cc8b97dad6aaca5700a98a938f4169",
+    "url": "/adidas/static/media/trending-4.70cc8b97.png"
+  }
+]);
