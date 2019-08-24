6d5e6d694615385c8ea2d09f756babb6e345e490
moaaz-bhnas <moaaz_bs@yahoo.com>
2019-08-24 21:50:46 +0200

    Finished theme

--- src/Pages/SingleCategory/SingleCategory.jsx
+++ src/Pages/SingleCategory/SingleCategory.jsx
@@ -3,6 +3,7 @@ import TopBanner from '../../Components/TopBanner/TopBanner2';
 import SubCategoriesList from '../../Components/Categories/SubCategoriesList';
 import Categories from '../../Components/Categories/Categories2';
 import Collections from '../../Components/Collections/Collections';
+import BottomBanner2 from '../../Components/BottomBanner/BottomBanner2';
 
 class SingleCategory extends Component {
   state = {  }
@@ -19,6 +20,8 @@ class SingleCategory extends Component {
         <Categories />
 
         <Collections />
+
+        <BottomBanner2 />
       </main>
     );
   }
