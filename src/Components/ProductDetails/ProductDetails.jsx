292aa2cfe09a211d231c39668c6e1ae5a07637c9
moaaz-bhnas <moaaz_bs@yahoo.com>
2019-09-02 21:08:42 +0200

    finished product details

--- src/Components/ProductDetails/ProductDetails.jsx
+++ src/Components/ProductDetails/ProductDetails.jsx
@@ -1,82 +1,85 @@
-import React, { memo, useState, useCallback } from 'react';
+import React, { PureComponent } from 'react';
 import './ProductDetails.scss';
 
-const ProductDetails = (props) => {
-  const { product } = props;
+const panels = [ 'highlights', 'description', 'specifications' ];
 
-  /* --- visible panel */
-  const panels = [ 'highlights', 'description', 'specifications' ];
+class ProductDetails extends PureComponent {
+  state = {  
+    activePanelName: 'description'
+  }
 
-  const [ activePanelName, setActivePanelName ] = useState('description');
-
-  const _setActivePanelName = useCallback((event, panel) => {
+  setActivePanelName = (event, panel) => {
     event.preventDefault();
-    setActivePanelName(panel);
-  });
+    this.setState({ activePanelName: panel });
+  }
 
-  /* panels */
-  const highlights = (
-    <div className="productHighlights">
-      <h4 className="productHighlights__title">Highlights</h4>
-    </div>
-  );
-  const description = (
-    <div className="productDescription">
-      <div className="productDescription__textContainer">
-        <h4 className="productDescription__title">{product.name}</h4>
-        <p className="productDescription__desc">{product.description}</p>
+  render() {
+    const { product } = this.props;
+    const { activePanelName } = this.state;
+
+    /* panels */
+    const highlights = (
+      <div className="productHighlights">
+        <h4 className="productHighlights__title">Highlights</h4>
       </div>
-      <img src={product.imgs[1]} alt={`${product.name} product`} className="productDescription__img"/>
-    </div>
-  );
-  const specs = (
-    <ul className="productSpecs">
-      {
-        Object.keys(product.specs).map((key) => (
-          <li className="productSpec" key={key}>
-            <b>{key}</b>: {product.specs[key]}
-          </li>
-        ))
-      }
-    </ul>
-  );
-
-  // determine active panel
-  const activePanel = 
-  (activePanelName === 'highlights')
-  ? highlights
-  : (activePanelName === 'description')
-  ? description
-  : specs
-
-
-  return (
-    <section className="productDetails container">
-      <h3 className="productDetails__title">Product Details</h3>
-
-      <nav className="productDetails__nav" aria-label="product details navigation">
-        <ul className="productDetails__navList">
-          {
-            panels.map((panel) => (
-              <li className="productDetails__navItem" key={panel}>
-                <a 
-                  href="#" 
-                  className={`productDetails__navLink${panel === activePanelName ? ' active' : ''}`}
-                  onClick={(event) => _setActivePanelName(event, panel)}
-                >
-                  {panel}
-                </a>
-              </li>
-            ))
-          }
-        </ul>
-      </nav>
-
-      <div className="productDetails__panel">
-        {activePanel}
+    );
+    const description = (
+      <div className="productDescription">
+        <div className="productDescription__textContainer">
+          <h4 className="productDescription__title">{product.name}</h4>
+          <p className="productDescription__desc">{product.description}</p>
+        </div>
+        <img src={product.imgs[1]} alt={`${product.name} product`} className="productDescription__img"/>
       </div>
-    </section>
-  );
+    );
+    const specs = (
+      <ul className="productSpecs">
+        {
+          Object.keys(product.specs).map((key) => (
+            <li className="productSpec" key={key}>
+              <b>{key}</b>: {product.specs[key]}
+            </li>
+          ))
+        }
+      </ul>
+    );
+
+    /* active panel */
+    const activePanel = 
+    (activePanelName === 'highlights')
+    ? highlights
+    : (activePanelName === 'description')
+    ? description
+    : specs;
+
+    return (
+      <section className="productDetails container">
+        <h3 className="productDetails__title">Product Details</h3>
+
+        <nav className="productDetails__nav" aria-label="product details navigation">
+          <ul className="productDetails__navList">
+            {
+              panels.map((panel) => (
+                <li className="productDetails__navItem" key={panel}>
+                  <a 
+                    href="#" 
+                    className={`productDetails__navLink${panel === activePanelName ? ' active' : ''}`}
+                    onClick={(event) => this.setActivePanelName(event, panel)}
+                  >
+                    {panel}
+                  </a>
+                </li>
+              ))
+            }
+          </ul>
+        </nav>
+
+        <div className="productDetails__panel">
+          {activePanel}
+        </div>
+      </section>
+    );
+  }
 }
 
-export default memo(ProductDetails);
+export default ProductDetails;
