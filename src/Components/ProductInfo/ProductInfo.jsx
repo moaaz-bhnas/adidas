14d03485aee3768cedc8d9d5b10bcfb1ec6af3cc
moaaz-bhnas <moaaz_bs@yahoo.com>
2019-09-02 21:14:40 +0200

    Finished product info

--- src/Components/ProductInfo/ProductInfo.jsx
+++ src/Components/ProductInfo/ProductInfo.jsx
@@ -1,116 +1,125 @@
-import React, { memo, useState } from 'react';
+import React, { PureComponent } from 'react';
 import './ProductInfo.scss';
 import Stars from './Stars';
 
-const ProductInfo = (props) => {
-  const { product } = props;
+class ProductInfo extends PureComponent {
+  state = {  
+    quantity: 1
+  }
 
-  const price = product.discount 
-  ? product.price - (product.price * 25 / 100)
-  : product.price;
+  setQuantity = (event) => {
+    this.setState({ quantity: Number(event.target.value) });
+  } 
 
-  /* --- quantity */
-  const [ quantity, setQuantity ] = useState(1);
+  render() {
+    const { product } = this.props;
+    const { quantity } = this.state;
 
-  return (
-    <div className="productInfo">
-      <div className="container">
-        <div className="d-flex">
-          {/* rating */}
-          <Stars rating={product.starsNum} />
+    const price = 
+    product.discount 
+    ? product.price - (product.price * 25 / 100)
+    : product.price;
 
-          {/* reviews */}
-          <a href="#" className="productInfo__reviewsLink">
-            Read all {product.reviews.length} reviews
-          </a>
-        </div>
+    return (
+      <div className="productInfo">
+        <div className="container">
+          <div className="d-flex">
+            {/* rating */}
+            <Stars rating={product.starsNum} />
 
-        {/* category */}
-        <div className="productInfo__category">
-          {`${product.gender}'s ${product.sport}`}
-        </div>
+            {/* reviews */}
+            <a href="#" className="productInfo__reviewsLink">
+              Read all {product.reviews.length} reviews
+            </a>
+          </div>
 
-        {/* name */}
-        <h4 className="productInfo__name">{product.name}</h4>
+          {/* category */}
+          <div className="productInfo__category">
+            {`${product.gender}'s ${product.sport}`}
+          </div>
 
-        {/* prices */}
-        <p className="productInfo__prices">
-          <span className={`productInfo__newPrice${product.discount ? ' discount' : null}`}>EGP {price}</span>
-          { product.discount && <span className="productInfo__oldPrice"> - EGP {product.price}</span> }
-        </p>
+          {/* name */}
+          <h4 className="productInfo__name">{product.name}</h4>
 
-        {/* promotional discount state */}
-        {
-          !product.promotionalDiscount &&
-          <p className="productInfo__promotionalDiscountState">
-            The product is excluded from all promotional discounts and offers.
+          {/* prices */}
+          <p className="productInfo__prices">
+            <span className={`productInfo__newPrice${product.discount ? ' discount' : null}`}>EGP {price}</span>
+            { product.discount && <span className="productInfo__oldPrice"> - EGP {product.price}</span> }
           </p>
-        }
 
-        {/* available colors */}
-        <h5 className="productInfo__colorsTitle">Available Colors</h5>
-
-        <ul className="productInfo__colorsNamesList">
+          {/* promotional discount state */}
           {
-            product.colors.map((color) => (
-              <li className="productInfo__colorName" key={color.name}>{color.name}</li>
-            ))
+            !product.promotionalDiscount &&
+            <p className="productInfo__promotionalDiscountState">
+              The product is excluded from all promotional discounts and offers.
+            </p>
           }
-        </ul>
 
-        <ul className="productInfo__colorsImgsList">
-          {
-            product.colors.map((color) => (
-              <li className="productInfo__colorImgContainer" key={color.name}>
-                <img src={color.img} alt={`${color.name} ${product.name}`} className="productInfo__colorImg"/>
-              </li>
-            ))
-          }
-        </ul>
+          {/* available colors */}
+          <h5 className="productInfo__colorsTitle">Available Colors</h5>
 
-        {/* size & quantity */}
-        <button 
-          className="productInfo__sizeChartBtn"
-          aria-pressed=""
-          aria-expanded=""
-        >
-          <i className="fas fa-ruler-combined mr-2" aria-hidden="true" /> 
-          <span className="text-underline">Size Chart</span> 
-        </button>
-
-        <div className="d-flex">
-          <select aria-label="select size" className="form-control productInfo__sizeSelect">
-            <option>Select Size</option>
+          <ul className="productInfo__colorsNamesList">
             {
-              product.sizes.map((size) => (
-                <option key={size}>{size}</option>
+              product.colors.map((color) => (
+                <li className="productInfo__colorName" key={color.name}>{color.name}</li>
               ))
             }
-          </select>
+          </ul>
 
-          <input 
-            type="number" 
-            min="1" 
-            className="form-control productInfo__quantityInput" 
-            value={quantity} 
-            onChange={(event) => setQuantity(event.target.value)}
-          />
-        </div> {/* flex container */}
+          <ul className="productInfo__colorsImgsList">
+            {
+              product.colors.map((color) => (
+                <li className="productInfo__colorImgContainer" key={color.name}>
+                  <img src={color.img} alt={`${color.name} ${product.name}`} className="productInfo__colorImg"/>
+                </li>
+              ))
+            }
+          </ul>
 
-        {/* add to bag & like */}
-        <div className="d-flex">
-          <button className="productInfo__addBtn">Add to bag</button>
-
-          <button className="productInfo__likeBtn" aria-label="like">
-            <i className="fas fa-heart fa-lg" aria-hidden="true" />
+          {/* size & quantity */}
+          <button 
+            className="productInfo__sizeChartBtn"
+            aria-pressed=""
+            aria-expanded=""
+          >
+            <i className="fas fa-ruler-combined mr-2" aria-hidden="true" /> 
+            <span className="text-underline">Size Chart</span> 
           </button>
+
+          <div className="d-flex">
+            <select aria-label="select size" className="form-control productInfo__sizeSelect">
+              <option>Select Size</option>
+              {
+                product.sizes.map((size) => (
+                  <option key={size}>{size}</option>
+                ))
+              }
+            </select>
+
+            <input 
+              type="number" 
+              min="1" 
+              className="form-control productInfo__quantityInput" 
+              value={quantity} 
+              onChange={this.setQuantity}
+            />
+          </div> {/* flex container */}
+
+          {/* add to bag & like */}
+          <div className="d-flex">
+            <button className="productInfo__addBtn">Add to bag</button>
+
+            <button className="productInfo__likeBtn" aria-label="like">
+              <i className="fas fa-heart fa-lg" aria-hidden="true" />
+            </button>
+          </div>
+          <p className="mt-3">
+            <i aria-hidden="true" className="fas fa-truck" /> Free shippings and free returns
+          </p>
         </div>
-        <p className="mt-3">
-          <i aria-hidden="true" className="fas fa-truck" /> Free shippings and free returns
-        </p>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
-export default memo(ProductInfo);
+export default ProductInfo;
