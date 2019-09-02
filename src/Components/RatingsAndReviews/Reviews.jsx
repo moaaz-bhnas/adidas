3edd075da1bee3acc884fb0b92691829698f3c48
moaaz-bhnas <moaaz_bs@yahoo.com>
2019-09-02 21:22:16 +0200

    finished reviews

--- src/Components/RatingsAndReviews/Reviews.jsx
+++ src/Components/RatingsAndReviews/Reviews.jsx
@@ -1,54 +1,57 @@
-import React, { useState, useCallback } from 'react';
+import React, { PureComponent } from 'react';
 import './Reviews.scss';
 import ReviewsList from './ReviewsList';
 
-const Reviews = (props) => {
-  const { reviews } = props;
-  
-  /* active category */
-  const categories = [ 'relevant', 'helpful', 'newest' ];
+const categories = [ 'relevant', 'helpful', 'newest' ];
 
-  const [ activeCategory, setActiveCategory ] = useState('helpful');
+class Reviews extends PureComponent {
+  state = {  
+    activeCategory: 'helpful'
+  }
 
-  const _setActiveCategory = useCallback((event, category) => {
+  setActiveCategory = (event, category) => {
     event.preventDefault();
-    setActiveCategory(category);
-  });
+    this.setState({ activeCategory: category });
+  };
 
-  const visibleReviews = reviews.filter((review) => review.category === activeCategory);
+  render() {
+    const { reviews } = this.props;
+    const { activeCategory } = this.state;
+    const visibleReviews = reviews.filter((review) => review.category === activeCategory);
 
-  return (
-    <div className="reviews">
-      <nav className="reviews__nav" aria-label="reviews navigation">
-        <p className="mb-2 text-uppercase w-100">Sort on</p>
-        <ul className="reviews__navList">
-          {
-            categories.map((category) => (
-              <li className="reviews__navItem" key={category}>
-                <a 
-                  href="#" 
-                  className={`reviews__navLink${category === activeCategory ? ' active' : ''}`}
-                  onClick={(event) => _setActiveCategory(event, category)}
-                >
-                  {category}
-                </a>
-              </li>
-            ))
-          }
-        </ul>
-      </nav>
-    
-      <ReviewsList reviews={visibleReviews} />
+    return (
+      <div className="reviews">
+        <nav className="reviews__nav" aria-label="reviews navigation">
+          <p className="mb-2 text-uppercase w-100">Sort on</p>
+          <ul className="reviews__navList">
+            {
+              categories.map((category) => (
+                <li className="reviews__navItem" key={category}>
+                  <a 
+                    href="#" 
+                    className={`reviews__navLink${category === activeCategory ? ' active' : ''}`}
+                    onClick={(event) => this.setActiveCategory(event, category)}
+                  >
+                    {category}
+                  </a>
+                </li>
+              ))
+            }
+          </ul>
+        </nav>
+      
+        <ReviewsList reviews={visibleReviews} />
 
-      <div className="reviews__btns">
-        <button className="reviews__loadBtn">
-          Load More <i className="fas fa-arrow-right" aria-hidden="true" />
-        </button>
+        <div className="reviews__btns">
+          <button className="reviews__loadBtn">
+            Load More <i className="fas fa-arrow-right" aria-hidden="true" />
+          </button>
 
-        <button className="reviews__writeReviewBtn">Write a Review</button>
+          <button className="reviews__writeReviewBtn">Write a Review</button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Reviews;
